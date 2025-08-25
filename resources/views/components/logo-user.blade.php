09004d7c20cf06902feda362d9d330f6873607fd
Nahocas <selenencmagallon@gmail.com>
2025-08-25 08:17:09 -0600

    subiendo para probar

---
+++ resources/views/components/logo-user.blade.php
@@ -0,0 +1,5 @@
+@props(['src'])
+
+<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <image href="{{ asset($src) }}" x="0" y="0" width="316" height="316" />
+</svg>
